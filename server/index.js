cfcf5400cb3259b1368c066965465954339f737b
tdyleuth <tony.dyleuth@gmail.com>
2021-05-11 20:06:25 -0700

    Setup Postgres DB

--- server/index.js
+++ server/index.js
@@ -2,12 +2,38 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const logger = require('morgan');
+const pool = require('./db');
 const PORT = 5000;
 
-//middleware
+//MIDDLEWARE
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
+
+//USER ROUTES//
+
+//create a user
+
+//get all users
+
+//get all a user
+
+//update a user
+
+//delete a user
+
+//Class ROUTES//
+
+//create a class
+
+//get all classes
+
+//get a class
+
+//update a class
+
+//delete a class
 
 app.listen(PORT, () => {
     console.log(`server has started on port ${PORT}`);
